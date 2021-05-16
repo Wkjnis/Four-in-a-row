431fb5002138cd7a563b191bc1d108b83cf13fb7
Wkjnis <nekit_52_1998@mail.ru>
2021-05-16 17:58:37 +0300

    player switching, animation of chips falling & etc

--- createFieldLayout.js
+++ createFieldLayout.js
@@ -1,15 +1,125 @@
-for(let i = 0; i <= 5; i++){
-    for(let j = 0; j <= 6; j++){
-        let elem = document.createElement('div');
-        elem.className = `field_${i}_${j}`;
-        elem.innerHTML = "<img src='field_fragment.svg'>";
-        document.querySelector(".field").append(elem);
+//Массив столбцов, с возможность бросать в них фишку
+let dropsArray = [true, true, true, true, true, true, true];
+
+//Массив игрового поля
+let fieldArray = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0]
+];
+
+/*
+    Объекты со свойствами игроков: цветом фишки:chipColor, именем: name,
+    типом игрока('player' или 'computer'): type, индксом для массива игрового поля: fieldArrayIndex
+*/
+const player1 = {
+    chipColor: "red",
+    name: "player1",
+    type: "player",
+    fieldArrayIndex: 1
+};
+
+const player2 = {
+    chipColor: "yellow",
+    name: "player2",
+    type: "player",
+    fieldArrayIndex: 2
+};
+
+//Переменная содержащая объект текущего игрока
+let currentPlayer = player1;
+
+createFieldLayout();
+createDropsLayout(currentPlayer);
+
+//Функция создает игровое поле
+function createFieldLayout() {
+    for( let i = 0; i <= 5; i++ ) {
+        for( let j = 0; j <= 6; j++ ) {
+            let fieldElem = document.createElement('div');
+            fieldElem.className = `field_${i}_${j}`;
+            fieldElem.insertAdjacentHTML("beforeend",`<img class="fieldFragment" src="field_fragment.svg" alt="fieldFragment">`);
+            document.querySelector(".field").append(fieldElem);
+        }
     }
 }
 
-for(let i = 0; i <= 6; i++){
-    let elem = document.createElement('div');
-    elem.className = `drop_${i}`;
-    elem.innerHTML = `<img class="chip" src="red_chip.svg" alt="chip">`;
-    document.querySelector(".drops").append(elem);
+//Функция создает область для броска фишек
+function createDropsLayout( currentPlayer ) {
+    for( let i = 0; i <= 6; i++ ) {
+        let dropsElem = document.createElement('div');
+        dropsElem.className = `drop_${i} started`;
+        dropsElem.setAttribute('data-index', i);
+        dropsElem.insertAdjacentHTML("beforeend", `<img class="chip" src="${currentPlayer.chipColor}_chip.svg" alt="${currentPlayer.chipColor}_chip">`);
+        dropsElem.onclick = () => { 
+            chipDrop(dropsElem.dataset.index);
+        };
+        document.querySelector(".drops").append(dropsElem);
+    }
 }
+
+//Функция реализует бросок фишки в стоблец dropsIndex во время хода игрока currentPlayer
+function chipDrop(dropsIndex) {
+    let chip = addChip (dropsIndex, currentPlayer );
+    let fieldCell = getHigestEmptyCell(dropsIndex);
+    //Позиционируем фишку
+    chip.style.top = 11*(fieldCell+1) + 3 +'vh';
+    //Добавляем фишку в массив игрового поля
+    fieldArray[fieldCell][dropsIndex] = currentPlayer.fieldArrayIndex;
+    disableDropIfFull(dropsIndex);
+    changePlayer();
+}
+
+// Функция добавляет фишку на поле в столбец dropsIndex цвета chipColor и возвращает ее
+function addChip (dropsIndex, currentPlayer ) {
+    let chip = document.createElement('div');
+    chip.className = `drop_${dropsIndex} added`;
+    chip.insertAdjacentHTML("beforeend", `<img class="chip_added" src="${currentPlayer.chipColor}_chip.svg" alt="${currentPlayer.chipColor}_chip">`);
+    document.querySelector(".drops").append(chip);
+    return chip;
+}
+
+//Функция возвращет номер первой пустой ячейке в стобце dropsIndex, если столбец заполнен, то null
+function getHigestEmptyCell (dropsIndex) {
+    for( let i = 5; i >= 0 ; i-- ) {
+        if( fieldArray[i][dropsIndex] === 0 ) {
+            return i;
+        }
+    }
+    return null;
+}
+
+// Функция отключает возможность броска фишки в столбец dropsIndex, если он заполнен
+function disableDropIfFull(dropsIndex) {
+    if( getHigestEmptyCell(dropsIndex) === null ) {
+        dropsArray[dropsIndex] = false; 
+        document.querySelector(`.drop_${dropsIndex}`).style = "pointer-events: none;";
+        document.querySelector(`.drop_${dropsIndex}`).querySelector('img').remove();
+        //Если хотим удалять обработчики событий:
+        //document.querySelector(`.drop_${dropsIndex}`).innerHTML = '';
+        if( dropsArray[dropsIndex] ) {
+            dropsArray[dropsIndex] = false;
+        }
+    }
+}
+
+function changePlayer() {
+    switch (currentPlayer) {
+        case player1:
+            currentPlayer = player2;
+            break;
+        case player2:
+            currentPlayer = player1;
+            break;
+    }
+    document.querySelectorAll('.started').forEach( (dropsElem) => {
+        if( dropsArray[dropsElem.dataset.index] ) {
+            dropsElem.querySelector('img').remove();
+            dropsElem.insertAdjacentHTML("beforeend", `<img class="chip" src="${currentPlayer.chipColor}_chip.svg" alt="${currentPlayer.chipColor}_chip">`);
+        }
+    } );
+}
+
